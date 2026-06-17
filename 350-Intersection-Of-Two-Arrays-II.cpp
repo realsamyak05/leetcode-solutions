82de159b1e40a82c572b7e506b40da44768912b1
samyakchaturvedi11@gmail.com <samyakchaturvedi11@gmail.com>
2026-06-17 17:49:22 +0530

    Optimized Intersection Of Two Arrays II

--- 350-Intersection-Of-Two-Arrays-II.cpp
+++ 350-Intersection-Of-Two-Arrays-II.cpp
@@ -1,19 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> intersect(vector<int> &nums1, vector<int> &nums2){
-    vector<int> v;
-    for(int i = 0; i < nums1.size(); i++){
-        for(int j = 0; j < nums2.size(); j++){
-            if (nums1[i] == nums2[j]){
-                nums2[j] = INT_MIN;
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0;
+        int j = 0;
+        vector<int> v;
+        while(i < nums1.size() && j < nums2.size()){
+            if (nums1[i] < nums2[j]){
+                i++;
+            }
+            else if (nums1[i] == nums2[j]){
                 v.push_back(nums1[i]);
-                break; 
+                i++;
+                j++;
+            }
+            else{
+                j++;
             }
         }
+        return v;
     }
-    return v;
-}
 
 int main(){
     int m, n;
